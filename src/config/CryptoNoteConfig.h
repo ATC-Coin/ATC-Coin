c545234d58208a49c4f8963b4007aa601f34959b
ATCCoin <92318834+ATC-Coin@users.noreply.github.com>
2022-01-15 17:52:30 +0000

    Mixin Changes

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -112,20 +112,20 @@ namespace CryptoNote
 
         const uint64_t MAXIMUM_MIXIN_V1 = 100;
 
-        const uint64_t MINIMUM_MIXIN_V2 = 0;
+        const uint64_t MINIMUM_MIXIN_V2 = 1;
 
-        const uint64_t MAXIMUM_MIXIN_V2 = 7;
+        const uint64_t MAXIMUM_MIXIN_V2 = 1;
 
-        const uint64_t MINIMUM_MIXIN_V3 = 0;
+        const uint64_t MINIMUM_MIXIN_V3 = 2;
 
-        const uint64_t MAXIMUM_MIXIN_V3 = 3;
+        const uint64_t MAXIMUM_MIXIN_V3 = 2;
 
         /* The heights to activate the mixin limits at */
         const uint32_t MIXIN_LIMITS_V1_HEIGHT = 0;
 
-        const uint32_t MIXIN_LIMITS_V2_HEIGHT = 1;
+        const uint32_t MIXIN_LIMITS_V2_HEIGHT = 1000;
 
-        const uint32_t MIXIN_LIMITS_V3_HEIGHT = 2;
+        const uint32_t MIXIN_LIMITS_V3_HEIGHT = 10000;
 
         /* The mixin to use by default with wallet software */
         /* DEFAULT_MIXIN_V0 is the mixin used before MIXIN_LIMITS_V1_HEIGHT is started */
