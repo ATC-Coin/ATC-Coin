3503460552c1bdaa372d6c32f68c5431b55d5824
ATCCoin <92318834+ATC-Coin@users.noreply.github.com>
2022-01-15 18:07:45 +0000

    Genesis Block&Timestamp

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -65,8 +65,8 @@ namespace CryptoNote
             "010a01ff000188f3b501029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210142694232c5b04151"
             "d9e4c27d31ec7a68ea568b19488cfcb422659a07a0e44dd5"; */
         
-        const char GENESIS_COINBASE_TX_HEX[] = "012801ff000188f3b501029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071840101ae723a5d883a4ade3cb332c6c8a697eb0b927d5a9f316984f52e2a81cd254f2b04000000000000000000000000000000000000000000000000000000000000000005000000000000000000000000000000000000000000000000000000000000000006b3f3942046c4a302b9fafa2782c88e3222598eba43eea411b49bfec6d5824802";
-        
+        const char GENESIS_COINBASE_TX_HEX[] = "012801ff000188f3b501029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071840101f5e8648437dc62b59925d75d53b78e9b817f2c1acc4e390d45ceb0fed6a067d304000000000000000000000000000000000000000000000000000000000000000005000000000000000000000000000000000000000000000000000000000000000006a98e1e653c57c62576b6fa48799d84a43f61c40b7807e6d907b2bb831df1180d";
+       
         static_assert(
             sizeof(GENESIS_COINBASE_TX_HEX) / sizeof(*GENESIS_COINBASE_TX_HEX) != 1,
             "GENESIS_COINBASE_TX_HEX must not be empty.");
@@ -74,7 +74,7 @@ namespace CryptoNote
         /* This is the unix timestamp of the first "mined" block (technically block 2, not the genesis block)
            You can get this value by doing "print_block 2" in TurtleCoind. It is used to know what timestamp
            to import from when the block height cannot be found in the node or the node is offline. */
-        const uint64_t GENESIS_BLOCK_TIMESTAMP = 1642265661;
+        const uint64_t GENESIS_BLOCK_TIMESTAMP = 1642269838;
 
         const size_t CRYPTONOTE_REWARD_BLOCKS_WINDOW = 100;
 
