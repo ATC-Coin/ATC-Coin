61816a5067e64bc9e3066b4cc6db000eb03b6fd1
maletazul <luissilva@maletazul.pt>
2022-03-19 18:35:43 +0000

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -261,12 +261,6 @@ namespace CryptoNote
         /* Block heights we are going to have hard forks at */
         const uint64_t FORK_HEIGHTS[] = {
             
-            2, // 0
-            3, // 1
-            4, // 2
-            5, // 3
-            6, // 4
-            7, // 5
             /*1, // 0
             2, // 1 */
         };
