7c6bbc2ce2b9cf4693398e11ab9c5f0d6f28d715
maletazul <luissilva@maletazul.pt>
2022-03-19 18:25:23 +0000

    init timestamps

--- src/config/CryptoNoteCheckpoints.h
+++ src/config/CryptoNoteCheckpoints.h
@@ -16,5 +16,9 @@ namespace CryptoNote
         const char *blockId;
     };
 
-    const std::initializer_list<CheckpointData> CHECKPOINTS = {};
+    const std::initializer_list<CheckpointData> CHECKPOINTS = {
+        {0, "3c8b586d801b6b93c9aec5291175486550213f5a3dfed496eed659b4b550263f"},
+        {2, "e174721c3436103d3db6c990dca190ea9f1c2db549ae58fd1ce12f46cfc8775e"},
+        {1000, "21dc4fd5b5d1e01aec99cf1fed40facbf61dbae31b949efbcce6a3b26ddffbd2"}};
+    };
 } // namespace CryptoNote
