23e4af5008c257833cd59f173d97fe8233881f87
maletazul <luissilva@maletazul.pt>
2022-04-12 08:25:02 +0100

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -75,7 +75,7 @@ namespace CryptoNote
         /* This is the unix timestamp of the first "mined" block (technically block 2, not the genesis block)
            You can get this value by doing "print_block 2" in TurtleCoind. It is used to know what timestamp
            to import from when the block height cannot be found in the node or the node is offline.1647354424 */
-        const uint64_t GENESIS_BLOCK_TIMESTAMP = 1648313941;
+        const uint64_t GENESIS_BLOCK_TIMESTAMP = 1649701765;
 
         const size_t CRYPTONOTE_REWARD_BLOCKS_WINDOW = 100;
 
