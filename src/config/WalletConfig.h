32e5c8648a3d2c3f20d69014467fd6bcaced9010
maletazul <luissilva@maletazul.pt>
2022-05-25 13:42:14 +0100

    0 mixin

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -62,6 +62,7 @@ namespace WalletConfig
 
     /* Is a mixin of zero disabled on your network? */
     const bool mixinZeroDisabled = true;
+    const uint64_t mixinZeroDisabledHeight = 0;
 
     /* If a mixin of zero is disabled, at what height was it disabled? E.g.
        fork height, or 0, if never allowed. This is ignored if a mixin of
